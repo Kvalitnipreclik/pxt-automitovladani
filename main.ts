c97b91537fce541cf6fc4f100770cd187abfd753
Kvalitnipreclik <115696189+Kvalitnipreclik@users.noreply.github.com>
2023-05-11 08:52:12 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
+
 // promene na hybani
-let interval = 20
+let interval = 50
 
 //promene na radio
 radio.setGroup(1)
 radio.setTransmitPower(7)
-radio.setFrequencyBand(0)
+radio.setFrequencyBand(50)
 
 //promene
 let xmod = 0
@@ -12,45 +13,57 @@ let zamek = false
 let posilaciString = ""
 
 input.onButtonPressed(Button.A, function () {
-   
-        if (zamek === true) {
-            zamek = false
-        } else {
-            zamek = true
-        }
-    
-    console.log(zamek)
 
-  
+    if (zamek === true) {
+        zamek = false
+        basic.showNumber(0)
+
+    } else {
+        zamek = true
+        basic.showNumber(1)
+
+    }
+
+
+
+
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(5)
+    
+    radio.sendString(String.fromCharCode(255))
+
+
 })
 
 
-function pack (x: number) {
-    
-    
-    xmod = input.acceleration(Dimension.X) + 1024
-    xmod = Math.floor(xmod / 8)
-    posilaciString = posilaciString + String.fromCharCode(x)
+function pack(xmod: number) {
+
+    xmod = Math.round(Math.map(xmod,-1023,1023,0,255))
+   
+    posilaciString = posilaciString + String.fromCharCode(xmod)
 
 }
 
 
 let x = 0
 let y = 0
-basic.forever(function() {
-    if(zamek){
+basic.forever(function () {
+    if (zamek) {
+        y = input.acceleration(Dimension.Y)
+        x = input.acceleration(Dimension.X)
 
-    
-    x = input.acceleration(Dimension.X)
-    y = input.acceleration(Dimension.Y)
-    pack(x)
-    pack(y)
-    console.log(posilaciString)
-    
-
-    radio.sendString(posilaciString)
+        pack(y)
+        pack(x)
+       
+        
+        radio.sendString(posilaciString)
         posilaciString = ""
-    basic.pause(interval)
+
+        basic.pause(interval)
     }
 })
-
+radio.onReceivedNumber(function(receivedNumber: number) {
+    basic.showNumber(6)
+})
